c4ea3ed69d9d5682c10595a9b15495298d82c48b
Rahulwagh07 <rahulwagh3774@gmail.com>
2025-01-08 13:09:31 +0530

    level-1 cors update

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: [process.env.FRONTEND_URL || 'http://localhost:3000'],
+  origin: [process.env.FRONTEND_URL || 'http://localhost:3000', process.env.ISITON_URL || ''],
   credentials: true
 }));
 
